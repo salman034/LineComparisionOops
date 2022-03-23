cabf6211d4b19531a9fc08abd73ec88c1e3bc9ac
unknown <you@example.com>
2022-03-23 13:24:35 +0530

    Added: Checking the Greater of Two Points

---
+++ src/com/bl/linecomparision/Line.java
@@ -0,0 +1,13 @@
+package com.bl.linecomparision;
+
+public class Line {
+    Point p1;
+    Point p2;
+    Point p3;
+    Point p4;
+
+    public float lineLength(int x, int y, int x1, int y1) {
+        float value = (float) Math.sqrt(Math.pow( (x - y) , 2 ) + Math.pow ( (x1 - y1 ), 2));
+        return value;
+    }
+}
---
+++ src/com/bl/linecomparision/Point.java
@@ -0,0 +1,17 @@
+package com.bl.linecomparision;
+
+import java.util.Scanner;
+
+public class Point {
+    public int x;
+    public int y;
+    public int x1;
+    public int y1;
+
+    public int sacanInput() {
+        Scanner scannerobj = new Scanner(System.in);
+        System.out.println("Enter the value of points");
+        int userInput  = scannerobj.nextInt();
+        return userInput;
+    }
+}
